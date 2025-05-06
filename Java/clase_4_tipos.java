8db2d7449817d1f87174b941baa848b134e4743f
Lucia Farina <luufarina@gmail.com>
2025-05-06 20:27:37 -0300

    Add java class 4

---
+++ Java/clase_4_tipos.java
@@ -0,0 +1,28 @@
+public class clase_4_tipos {
+    public static void main(String[] args) {
+        float numFloat = 3.4028538f;
+        System.out.println(numFloat);
+        System.out.println("El valor minimo de float: " + Float.MIN_VALUE);
+        System.out.println("El valor maximo de float: " + Float.MAX_VALUE);
+
+        // double numDouble = 1.752115745;
+        System.out.println("El valor minimo de double: " + Double.MIN_VALUE);
+        System.out.println("El valor maximo de double: " + Double.MAX_VALUE);
+
+        // byte numEntero = 10;
+        System.out.println("El valor minimo de byte: " + Byte.MIN_VALUE);
+        System.out.println("El valor maximo de byte: " + Byte.MAX_VALUE);
+
+        // short numEnteroShort = 32767;
+        System.out.println("El valor minimo de short: " + Short.MIN_VALUE);
+        System.out.println("El valor maximo de short: " + Short.MAX_VALUE);
+
+        // int numeroEnteroInt = 2147483647;
+        System.out.println("El valor minimo de Integer: " + Integer.MIN_VALUE);
+        System.out.println("El valor maximo de Integer: " + Integer.MAX_VALUE);
+
+        // long numeroEnteroLong = 10;
+        System.out.println("El valor minimo de Long: " + Long.MIN_VALUE);
+        System.out.println("El valor maximo de Long: " + Long.MAX_VALUE);
+    }
+}
